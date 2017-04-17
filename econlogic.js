a17539aacb3ebf5224c4b681d2996d0b85051220
Nicole Watkins <nicole.watkins@detroitlabs.com>
2017-04-16 21:59:55 -0400

    Creating supply-demand graph with inverted axes

--- econlogic.js
+++ econlogic.js
@@ -1,1 +1,28 @@
-document.write("Hello, world!");
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Price', 'Demand', 'Supply'],
+          [0,   100,      0,],
+          [10,  75,      25,],
+          [20,  50,      50,],
+          [30,  25,       75,],
+          [40,  0,      100,]
+        ]);
+
+        var options = {
+          title: 'Supply Demand',
+          selectionMode: 'multiple',
+          orientation: 'vertical',
+          explorer: { axis: 'horizontal' },
+          focusTarget: 'datum',
+          tooltip: {trigger: 'selection'},
+          aggregationTarget: 'category',
+          hAxis: {title: 'Quantity', minValue: 0,  titleTextStyle: {color: '#333'}},
+          vAxis: {title: 'Price($)', minValue: 0, format: 'currency'}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
